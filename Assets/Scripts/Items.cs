51c1a97c8d1afe5b5b4db0fbf7bce59f51a4d066
huner15 <mchimp@hotmail.com>
2016-01-30 13:21:36 -0800

    merge

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -34,6 +34,16 @@ public class Items : MonoBehaviour
                             tags.Add(l[2]);
                         item.tags = tags;
                     }
+					else if (l[0].Equals("sprite"))
+                    {
+                        Sprite sprite = Resources.Load<Sprite>(l[2]);
+                        GameObject gameObject = new GameObject();
+                        gameObject.AddComponent<SpriteRenderer>();
+                        SpriteRenderer SR = gameObject.GetComponent<SpriteRenderer>();
+                        SR.sprite = sprite;
+                        item.sprite = Instantiate(gameObject) as GameObject;
+                        item.sprite.SetActive(false);
+                    }
                     else if (int.TryParse(l[2], out x))
                         item.GetType().GetProperty(l[0]).SetValue(item, x, null);
                     else if (bool.TryParse(l[2], out b))
@@ -69,6 +79,7 @@ public class Items : MonoBehaviour
             canWield = wield;
             isArmor = armor;
         }
+		public GameObject sprite { get; set; }
         public int value { get; set; }
         public string name { get; set; }
         public List<string> tags { get; set; }
@@ -92,4 +103,15 @@ public class Items : MonoBehaviour
         }
         return itemsWithTag[Random.Range(0, itemsWithTag.Count)];
     }
+	// Gets an item with the given name
+    public static Item getItemWithName(string name)
+    {
+        foreach (Item item in items)
+        {
+            if (item.name.Equals(name))
+                return item;
+        }
+        return null;
+    }
+	
 }
